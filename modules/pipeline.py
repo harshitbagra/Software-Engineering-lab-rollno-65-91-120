ad0169e9283f23c585a24313d7c8ac91b0b28c0e
harshitbagra <harshitbagra339@gmail.com>
2026-03-01 21:56:11 +0530

    Add files via upload

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -1,6 +1,5 @@
 class CICDPipeline:
-    def __init__(self, pipeline_id):
-        self.pipeline_id = pipeline_id
+    def __init__(self):
         self.status = "INITIALIZED"
 
     def start_pipeline(self):
@@ -8,31 +7,6 @@ class CICDPipeline:
         self.status = "RUNNING"
         return self.status
 
-    def update_status(self, status):
-        self.status = status
-        print("Pipeline status:", self.status)
-
-
-class BuildManager:
-    def __init__(self):
-        self.build_status = "NOT_STARTED"
-
-    def fetch_source_code(self):
-        print("Fetching source code")
-        return True
-
-    def build_application(self):
-        print("Building application")
-        self.build_status = "SUCCESS"
-        return self.build_status
-
-
-if __name__ == "__main__":
-    pipeline = CICDPipeline(101)
-    build_manager = BuildManager()
-
-    pipeline.start_pipeline()
-
-    if build_manager.fetch_source_code():
-        result = build_manager.build_application()
-        pipeline.update_status(result)
+    def update_status(self, new_status):
+        self.status = new_status
+        print("Pipeline Status:", self.status)
